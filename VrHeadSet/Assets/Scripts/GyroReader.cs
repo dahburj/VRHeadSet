871cb10f0e1e16320cb68928a49f089ffd93795c
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-12-15 22:34:08 +0200

    done splitting

--- VrHeadSet/Assets/Scripts/GyroReader.cs
+++ VrHeadSet/Assets/Scripts/GyroReader.cs
@@ -4,20 +4,37 @@ using System.IO.Ports;
 
 public class GyroReader : MonoBehaviour {
 	public float speed;
+	public float gyrox;
+	public float gyroy;
+	public float gyroz;
 	public float amountx;
 	public float amounty;
 	public float amountz;
+	public string [] axis = new string[3];
 
 	// Connect To Serial Port
-	SerialPort serialPort = new SerialPort("/dev/tty.usbmodem1421", 9600);
+	//MAC
+	//SerialPort serialPort = new SerialPort("/dev/tty.usbmodem1421", 9600);
+	//WINDOWS
+	SerialPort serialPort = new SerialPort("COM6", 9600);
+
 	
 	// Use this for initialization
 	void Start () 
 	{
+		splitAxis("23,15,99");
 		serialPort.Open ();
 		serialPort.ReadTimeout = 1;
 	}
-	
+	public void splitAxis(string sample){
+		axis = sample.Split(',');
+		gyrox=float.Parse(axis[0]);
+		gyroy=float.Parse(axis[1]);
+		gyroz=float.Parse(axis[2]);
+		Debug.Log(gyrox);
+		Debug.Log(gyroy);
+		Debug.Log(gyroz);
+	}
 	// Update is called once per frame
 	void Update () 
 	{
